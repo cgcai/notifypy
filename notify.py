fd761fb3b12a06c12062ef824515d4f87c781213
Camillus Gerard Cai <cgcai@qxcg.net>
2013-06-19 21:45:10 +0800

    Basic notification.

--- notify.py
+++ notify.py
@@ -1,42 +1,46 @@
 #!/usr/bin/env python
 
+# Sends an SMS to a target number when run.
+# This script should be chained in a shell command:
+# $ do_something_long && sleep 5 && ./notify hoiio.id "something is done!"
+# 
+# The id file should consist of the following lines with no leading or trailing characters except whitespace.
+# 1. App Id
+# 2. Access Token
+# 3. Target Number
+# Additional lines are ignored.
+#
+# Usage: notify.py <id file> <message>
+#
+# --Xofel.
+
 import sys
-import os
-import time
 from urllib import urlencode
 import urllib2
+import json
 
 # Exit codes.
 _EXIT_SYNTAX = 1
 _HOIIO_SMS_API = "https://secure.hoiio.com/open/sms/send"
 
-# Script globals.
-_def_dest = ""
-_hoiio_appid = ""
-_hoiio_access_token = ""
-
 def main():
-	send_sms("xyz", "abc", "82998500", "hello world")
-	# (should_exec, command, should_wait, duration, idf, message = parse_args()
+	id_file, message = parse_args()
+	with open(id_file, "r") as f:
+		appid = f.readline().strip()
+		token = f.readline().strip()
+		dest = f.readline().strip()
 
-	# if _should_exec:
-	# 	proc = os.system(_command)
-	# 	print "Spawned process:", str(proc)
-
-	# if _should_wait:
-	# 	print "Sleeping for", _wait_duration, "seconds"
-	# 	time.sleep(_wait_duration)
-
-	# print "Notifying..."
-	# success = send_sms()
-
-	# if not success:
-	# 	print "Failed to send SMS."
-	# else:
-	# 	print "SMS sent."
+	send_sms(appid, token, dest, message)
 
 def parse_args():
-	pass
+	if len(sys.argv) != 3:
+		print usage()
+		sys.exit(_EXIT_SYNTAX)
+
+	id_file = str(sys.argv[1])
+	message = str(sys.argv[2])
+
+	return (id_file, message)
 
 def send_sms(appid, token, dest, message):
 	request_data = {
@@ -48,13 +52,16 @@ def send_sms(appid, token, dest, message):
 
 	url = _HOIIO_SMS_API + "?" + urlencode(request_data)
 	req = urllib2.Request(url)
-	resp = urllib2.urlopen(req).read()
+	resp_data = urllib2.urlopen(req).read()
 
-	print resp
+	resp = json.loads(resp_data)
+	if "status" in resp.keys() and resp["status"] == "success_ok":
+		print "SMS sent."
+	else:
+		print "Failed to send SMS."
 
 def usage():
-	print "usage: notify.py [-e <command>] [-t <time, seconds>] <id file> <message>"
-	sys.exit(_EXIT_SYNTAX)
+	return "usage: notify.py <id file> <message>"
 
 if __name__ == '__main__':
 	main()

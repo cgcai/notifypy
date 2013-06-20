a66fb13652313bdd4ee3a071d11f969be1c13f06
Camillus Gerard Cai <cgcai@qxcg.net>
2013-06-20 11:00:23 +0700

    More options.

--- notify.py
+++ notify.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python
 
-# Sends an SMS to a target number when run.
-# This script should be chained in a shell command:
-# $ do_something_long && sleep 5 && ./notify hoiio.id "something is done!"
-# 
-# The id file should consist of the following lines with no leading or trailing characters except whitespace.
-# 1. App Id
-# 2. Access Token
-# 3. Target Number
-# Additional lines are ignored.
-#
-# --Xofel.
+# Sends an SMS to a target number when run. --Xofel.
 
 from urllib import urlencode
 import urllib2
@@ -31,14 +21,15 @@ def main():
 	parser.add_argument("-e", "--execute", help="command to execute using this script's permissions")
 	parser.add_argument("-w", "--wait", type=int, help="time in seconds to wait before notifying. If specified with --execute, the timer will start after the spawned process completes")
 	parser.add_argument("-s", "--stingy", action="store_true", help="refuse to send SMS if len(message) > 160. Always prints refusal regardless of --verbose")
-	parser.add_argument("identity", help="identity file to use")
+	parser.add_argument("-i", "--identity", default="~/.notifypy", help="identity file to use. Default: ~/.notifypy")
 	parser.add_argument("message", help="content of SMS to send")
 	args = parser.parse_args()
 
 	if args.verbose:
 		_VERBOSE = True
 
-	with open(args.identity, "r") as f:
+	id_path = os.path.expanduser(args.identity)
+	with open(id_path, "r") as f:
 		appid = f.readline().strip()
 		log("App ID: " + appid)
 		token = f.readline().strip()
@@ -71,7 +62,7 @@ def send_sms(appid, token, dest, message, testmode=False):
 
 	url = _HOIIO_SMS_API + "?" + urlencode(request_data)
 	if testmode:
-		print url
+		log("Test Mode: " + url, always=True)
 	else:
 		req = urllib2.Request(url)
 		resp_data = urllib2.urlopen(req).read()
